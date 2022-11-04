5f010a6b75314c50dc36a2899810ba59fefcd410
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-04 11:53:34 +0100

    storageStorage works

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,32 @@ import { INoun } from './interfaces';
 import * as tools from './tools';
 
 const nounsUrl = 'https://edwardtanguay.vercel.app/share/germanNouns.json';
+const localStorageVariable = 'german-noun-site-app-state';
 
 function App() {
 	const [nouns, setNouns] = useState<INoun[]>([]);
 
 	useEffect(() => {
 		(async () => {
-			const response = await axios.get(nounsUrl);
-			const rawNouns = response.data;
+			const localStorageNouns =
+				localStorage.getItem(localStorageVariable);
 			let _nouns: INoun[] = [];
-			rawNouns.forEach((rawNoun: any) => {
-				const _noun: INoun = {
-					...rawNoun,
-					isOpen: false,
-					isLearned: false
-				};
-				_nouns.push(_noun);
-			});
-			_nouns = tools.randomizeArray(_nouns);
+			if (localStorageNouns === null) {
+				const response = await axios.get(nounsUrl);
+				const rawNouns = response.data;
+				_nouns = [];
+				rawNouns.forEach((rawNoun: any) => {
+					const _noun: INoun = {
+						...rawNoun,
+						isOpen: false,
+						isLearned: false
+					};
+					_nouns.push(_noun);
+				});
+				_nouns = tools.randomizeArray(_nouns);
+			} else {
+				_nouns = JSON.parse(localStorageNouns);
+			}
 			setNouns(_nouns);
 		})();
 	}, []);
@@ -32,8 +40,15 @@ function App() {
 		<div className="App">
 			<h1>German Noun Site</h1>
 			{/* <p>You have learned {(nouns.filter(m => m.isLearned)).length} of {nouns.length} nouns.</p> */}
-			<p>You have learned {nouns.reduce((total,noun) => total + (noun.isLearned ? 1 : 0), 0)} of {nouns.length} nouns.</p>
-			<Nouns nouns={nouns} setNouns={setNouns}/>
+			<p>
+				You have learned{' '}
+				{nouns.reduce(
+					(total, noun) => total + (noun.isLearned ? 1 : 0),
+					0
+				)}{' '}
+				of {nouns.length} nouns.
+			</p>
+			<Nouns nouns={nouns} setNouns={setNouns} />
 		</div>
 	);
 }
