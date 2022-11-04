568bc2be4efdac8cf4b138e926d0eac2f46854b3
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-04 09:52:45 +0100

    site gets data from nouns

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import './App.scss'
+import { useState, useEffect } from 'react';
+import './App.scss';
+import axios from 'axios';
+
+const nounsUrl = 'https://edwardtanguay.vercel.app/share/germanNouns.json';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>German Noun Site</h1>
-      <p>Welcome to this site.</p>
-    </div>
-  )
+	const [nouns, setNouns] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(nounsUrl);
+      const _nouns = response.data;
+      setNouns(_nouns);
+		})();
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>German Noun Site</h1>
+			<p>There are {nouns.length} nouns.</p>
+		</div>
+	);
 }
 
-export default App
+export default App;
